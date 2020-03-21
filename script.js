35b419f984cf96990c6456bac7ccebdcdec01839
Shaw Coney <rockconey@gmail.com>
2020-03-21 17:01:11 -0500

    unfinished

--- script.js
+++ script.js
@@ -3,15 +3,26 @@ window.addEventListener("load", function () {
         response.json().then(function(json) {
             console.log(json);
             let jsonObj = json;
-            let newDiv;
 
-            for (let i = 0; i < jsonObj.length; i++) {
-                
-               
-                
+            // pro tip: you need to put a string into innerHtml, so my approach would be to create your own array, append to it, 
+            // then turn it into a string when you apply!
+
+            for (let i = jsonObj.length - 1; i >=0; i --) {
                 const div = document.getElementById("container");
-                
-                div.innerHTML = div.innerHTML + `
+               
+                let astroArr = [];
+                astroArr.push(jsonObj[i]);
+                console.log(astroArr);
+                if (jsonObj[i].firstName) {
+                    
+                    document.body.append(astroArr);
+
+                    //Problem solving: If I append the div I get one astronaut. If I append text, I get one astronaut, and then literally all the innerHTML for the other
+                    // astronauts. if I append the astroArr then I get one in the appropriate format, and get [object Object] appended after.
+
+                    
+
+                    div.innerHTML = /*div.innerHTML +*/ `
             
                 <div class="astronaut">
                     <div class="bio">
@@ -25,8 +36,36 @@ window.addEventListener("load", function () {
                    <img class="avatar" src=${jsonObj[i].picture}>
                 </div>        
                `
-               
+                }
             }
+            
+
+            // for (let i = 0; i < jsonObj.length; i++) {
+
+                
+
+
+                
+               
+                
+            //     const div = document.getElementById("container");
+                
+            //     div.innerHTML = /*div.innerHTML +*/ `
+            
+            //     <div class="astronaut">
+            //         <div class="bio">
+            //         <h3>${jsonObj[i].firstName} ${jsonObj[i].lastName}</h3>
+            //         <ul>
+            //             <li>Hours in Space: ${jsonObj[i].hoursInSpace}</li>
+            //             <li>Active: ${jsonObj[i].active}</li>
+            //             <li>Skills: ${jsonObj[i].skills}</li>
+            //         </ul>
+            //        </div>
+            //        <img class="avatar" src=${jsonObj[i].picture}>
+            //     </div>        
+            //    `
+               
+           // }
 
             
 
